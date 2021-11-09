b9687d0d5ad8e8ab2812e7903364deabcd96fc11
ubcruzl <40645692+ubcruzl@users.noreply.github.com>
2021-11-09 12:53:51 -0800

    CORRECTION CHANGES

---
+++ Project7_scope_time_function/JS/main.js
@@ -0,0 +1,11 @@
+//
+var X=10;
+
+function addNumbers_1(){             //declare function
+    document.write(20+X+"<br>");
+}
+function addNumbers_2(){             //declare function
+    document.write(X+100);
+}
+addNumbers_1();
+addNumbers_2();
